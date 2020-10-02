e7fc4b6b0e5f08ba5e06dddee72de575f590d368
Debra Wolford <debra.wolford@gmail.com>
2020-10-02 21:04:06 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51HXtUSESZVPCI9L2w3toInYlUdFxUQxlagElJc135DDkCgAB59hDGc482ftZ71jwT8o7pgmIJ2oSDVnNGTySpY5500i6wB3bCc',
+        'client_secret': 'test_client_secret',
     }
 
     return render(request, template, context)
