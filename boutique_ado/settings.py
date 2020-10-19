927cd0cab0c5853ff750545bae0a87f384c73c8a
Debra Wolford <debra.wolford@gmail.com>
2020-10-19 18:26:13 +0000

    Prepared Procfile and Settings.py

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '9(l5ozx@m9lv0w_6h5q$zs+#^)-vjxk$q%qsu%wj9vou+8vn+a'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['djw-boutique-ado.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -118,13 +118,17 @@ WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-       'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': BASE_DIR / 'db.sqlite3',
-     }
- }
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
